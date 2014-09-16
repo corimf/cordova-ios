97957e164d15854b26816a390cfcde9fa825648e
Shazron Abdullah <shazron@apache.org>
2014-09-15 23:04:38 -0700

    CB-7560 - Tel and Mailto links don't work in iframe
    (cherry picked from commit cf367cb49916196ce6009b54c4a6dfa9779be20c)
    
    Conflicts:
    	CordovaLibTests/CDVWebViewDelegateTests.m

--- CordovaLib/Classes/CDVWebViewDelegate.m
+++ CordovaLib/Classes/CDVWebViewDelegate.m
@@ -193,6 +193,17 @@ static NSString *stripFragment(NSString* url)
     }
 }
 
+- (BOOL)shouldLoadRequest:(NSURLRequest*)request
+{
+    NSString* scheme = [[request URL] scheme];
+
+    if ([scheme isEqualToString:@"mailto"] || [scheme isEqualToString:@"tel"]) {
+        return YES;
+    }
+
+    return [NSURLConnection canHandleRequest:request];
+}
+
 - (BOOL)webView:(UIWebView*)webView shouldStartLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType
 {
     BOOL shouldLoad = YES;
@@ -251,7 +262,7 @@ static NSString *stripFragment(NSString* url)
         } else {
             // Deny invalid URLs so that we don't get the case where we go straight from
             // webViewShouldLoad -> webViewDidFailLoad (messes up _loadCount).
-            shouldLoad = shouldLoad && [NSURLConnection canHandleRequest:request];
+            shouldLoad = shouldLoad && [self shouldLoadRequest:request];
         }
         VerboseLog(@"webView shouldLoad=%d (after) isTopLevelNavigation=%d state=%d loadCount=%d", shouldLoad, isTopLevelNavigation, _state, _loadCount);
     }
